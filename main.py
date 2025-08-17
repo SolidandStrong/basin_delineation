38955021af4cb254212764a8a2c62a4d0ebd37f5
SolidandStrong <104705946+SolidandStrong@users.noreply.github.com>
2025-08-17 13:52:32 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import Draw
-
+import Compare_dhp2dic
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
+
+    # validation
+    # 计算RiverLakeBasins中湖泊的上游面积
+    Compare_dhp2dic.sbatch_main_calculate_upstreamarea_GNWL('\AllContinent','\dic')
+    # 计算Lake-Topo中湖泊的上游面积
+    Compare_dhp2dic.sbatch_main_calculate_upstreamarea_TopoCat('\Catchments','\LakeUpstreamArea')
+    # 整和RiverLakeBasins和Lake-Topo中共同的湖泊，得到最终的验证结果
+    Compare_dhp2dic.merge_csv_TopoCat('\LakeUpstreamArea','\TopoCatArea.csv')
+
+
 
     # Draw Figure 6
     Draw.drawFig1('Global_statis1.csv')
